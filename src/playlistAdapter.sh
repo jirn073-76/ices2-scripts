d7f1fe11b43294f0543ba8a90466461375969ec7
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-08 13:45:45 +0100

    Added check for invalid filenames

--- src/playlistAdapter.sh
+++ src/playlistAdapter.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # This script needs to be called with the playlist file as a cmdline parameter
 # and referenced as <param name="program">playlistAdapter.sh</param> in the ices2 config
-
 dir=$1
-currentlinecount=$(cat /home/admin/radioScripts/currentLine)
-linecount=$(wc -l $dir | cut -f1 -d' ')
-currentline=$(head -$currentlinecount $dir | tail -1)
-currentlinecount=$((currentlinecount + 1))
+
+while [ ! -f "$currentline" ];
+do
+    currentlinecount=$(cat /home/admin/radioScripts/currentLine)
+    linecount=$(wc -l $dir | cut -f1 -d' ')
+    currentline=$(head -$currentlinecount $dir | tail -1)
+    currentlinecount=$((currentlinecount + 1))
+#   echo "$currentline lc: $linecount currentlinecount: $currentlinecount"
+done
 
 if [ $currentlinecount -gt $linecount ]
 then

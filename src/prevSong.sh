44ff60b268d72a506e45624c7e3c7c172ce63ab0
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-08 18:40:16 +0100

    Removed redundant echos, fixed sed perm issues and handled the case where linecount can be < 1

--- src/prevSong.sh
+++ src/prevSong.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 dir=$1
-
+currentline="sentinel.file.should.not.exist"
 o_linecount=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"current_line"/ {print}' | sed 's/\"current_line\"://g;s/\"//g;s/{//g;s/^ *//g')
 o_currentline=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"currently_playing"/ {print}' | sed 's/\"currently_playing\"://g;s/\"//g;s/^ *//g')
-
-echo "$o_currentline lc: $o_linecount"
 
 while [ ! -f "$currentline" ];
 do
     currentlinecount=$(cat /home/admin/radioScripts/currentLine)
     linecount=$(wc -l $dir | cut -f1 -d' ')
+    currentlinecount=$((currentlinecount - 1))
+    if [ $currentlinecount -lt 1 ]
+    then
+        currentlinecount=$linecount
+    fi
     currentline=$(head -$currentlinecount $dir | tail -1)
-    currentlinecount=$((currentlinecount -1 ))
 done
 
-if [ $currentlinecount -lt 1 ]
-then
-    currentlinecount=$linecount
-fi
+cp /home/admin/radioScripts/currentLine /tmp
+sed -i /tmp/currentLine -e "s/[0-9]*/$currentlinecount/g"
+cat /tmp/currentLine > /home/admin/radioScripts/currentLine
 
-sed -i "s/[0-9]*/$currentlinecount/g" currentLine
-#echo $linecount
 echo "{\"current_line\": $currentlinecount, \"total_lines\": $linecount, \"next_up\": \"$(head -$((currentlinecount)) $dir | tail -1)\",  \"currently_playing\": \"$o_currentline\", \"song_info\": $(ffprobe -v quiet -print_format json -s$echo $currentline")}" > "/var/www/radio/currentlyPlaying.json"
-#echo $currentlinecount

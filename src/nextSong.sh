4336323321528ddb9926ab9abacd301566285b83
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-08 18:38:32 +0100

    Removed redundant outputs to stdout and fixed permission issues with sed

--- src/nextSong.sh
+++ src/nextSong.sh
@@ -4,8 +4,6 @@ dir=$1
 o_linecount=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"current_line"/ {print}' | sed 's/\"current_line\"://g;s/\"//g;s/{//g;s/^ *//g')
 o_currentline=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"currently_playing"/ {print}' | sed 's/\"currently_playing\"://g;s/\"//g;s/^ *//g')
 
-echo "$o_currentline lc: $o_linecount"
-
 while [ ! -f "$currentline" ];
 do
     currentlinecount=$(cat /home/admin/radioScripts/currentLine)
@@ -20,8 +18,7 @@ then
     shuf $dir > "${dir}.shuffled"
     $(mv "${dir}.shuffled" $dir)
 fi
-
-sed -i "s/[0-9]*/$currentlinecount/g" currentLine
-#echo $linecount
+cp /home/admin/radioScripts/currentLine /tmp
+sed -i /tmp/currentLine -e "s/[0-9]*/$currentlinecount/g"
+cat /tmp/currentLine > /home/admin/radioScripts/currentLine
 echo "{\"current_line\": $currentlinecount, \"total_lines\": $linecount, \"next_up\": \"$(head -$((currentlinecount)) $dir | tail -1)\",  \"currently_playing\": \"$o_currentline\", \"song_info\": $(ffprobe -v quiet -print_format json -s$echo $currentline")}" > "/var/www/radio/currentlyPlaying.json"
-#echo $currentlinecount

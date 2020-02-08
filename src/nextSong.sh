b00945c87ca42ac88c034e34902fd92a6a6cfd37
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-08 16:13:58 +0100

    Gracefully lines up next song (and updates json accordingly)

---
+++ src/nextSong.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dir=$1
+
+o_linecount=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"current_line"/ {print}' | sed 's/\"current_line\"://g;s/\"//g;s/{//g;s/^ *//g')
+o_currentline=$(cat /var/www/radio/currentlyPlaying.json | awk -F=":" -v RS="," '$1~/"currently_playing"/ {print}' | sed 's/\"currently_playing\"://g;s/\"//g;s/^ *//g')
+
+echo "$o_currentline lc: $o_linecount"
+
+while [ ! -f "$currentline" ];
+do
+    currentlinecount=$(cat /home/admin/radioScripts/currentLine)
+    linecount=$(wc -l $dir | cut -f1 -d' ')
+    currentline=$(head -$currentlinecount $dir | tail -1)
+    currentlinecount=$((currentlinecount + 1))
+done
+
+if [ $currentlinecount -gt $linecount ]
+then
+    currentlinecount=1
+    shuf $dir > "${dir}.shuffled"
+    $(mv "${dir}.shuffled" $dir)
+fi
+
+sed -i "s/[0-9]*/$currentlinecount/g" currentLine
+#echo $linecount
+echo "{\"current_line\": $currentlinecount, \"total_lines\": $linecount, \"next_up\": \"$(head -$((currentlinecount)) $dir | tail -1)\",  \"currently_playing\": \"$o_currentline\", \"song_info\": $(ffprobe -v quiet -print_format json -s$echo $currentline")}" > "/var/www/radio/currentlyPlaying.json"
+#echo $currentlinecount

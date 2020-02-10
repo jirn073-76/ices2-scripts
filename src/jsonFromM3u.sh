504a03873af5b10d360312621aa6e840e00b265b
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-10 23:07:49 +0100

    Proper handling of temporary directories

--- src/jsonFromM3u.sh
+++ src/jsonFromM3u.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 dir=$1
 ind=1
-tmpDir="/tmp/jsonFromM3u.tmp"
+tmpDir=`mktemp`
 touch $tmpDir
 echo "{\"songlist\": [" > $tmpDir
 
 while IFS= read -r line
 do
-  echo "\"$ind: $line\"," >> $tmpDir
+  echo "{\"index\": \"$ind\", \"filename\": \"$line\"}," >> $tmpDir
   ind=$(($ind+1))
 done < "$dir"
 sed -i '$ s/.$//' $tmpDir
 echo "]}" >> $tmpDir
-
-cp /tmp/jsonFromM3u.tmp /var/www/radio/jsonFromM3u.json
+cat $tmpDir > /var/www/radio/jsonFromM3u.json

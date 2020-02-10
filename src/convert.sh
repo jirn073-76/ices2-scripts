505a3cea7dab965ce2fce32edb09f976bd24e57f
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-10 23:14:00 +0100

    Switched to libvorbis encoding

--- src/convert.sh
+++ src/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script converts all audio files in the execution directory to .ogg libvorbis encoded files with a 320k bitrate
 # Also removes .flac extensions
-find . -type f -name "*.*" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -c:a libmp3lame -ab 320k -y "${FILE%.flac}.mp3";' _ '{}' \;
+find . -type f -name "*.*" -exec bash -c 'FILE="$1"; ffmpeg -i "${FILE}" -vn -c:a libvorbis -ab 320k -y "${FILE%.mp3}.ogg";' _ '{}' \;
